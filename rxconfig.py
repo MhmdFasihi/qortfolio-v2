57c4fe83ef236593397cff97b98d5c1f8c1fb87d
MhmdFasihi <mhmdfasihi@users.noreply.github.com>
2025-09-04 15:59:09 +0330

    reflex config

--- rxconfig.py
+++ rxconfig.py
@@ -1,5 +1,7 @@
 import reflex as rx
 
+# Explicitly disable the default sitemap plugin to silence warnings.
 config = rx.Config(
     app_name="qortfolio_v2",
+    disable_plugins=["reflex.plugins.sitemap.SitemapPlugin"],
 )
